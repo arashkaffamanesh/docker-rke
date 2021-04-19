9b626d88c120c08164371dc16b5bd1b832cde653
Alexander Giesa <alex@77g.de>
2021-04-19 09:35:59 +0200

    feat/more checks added

--- extended-cleanup-rancher2.sh
+++ extended-cleanup-rancher2.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
-docker rm -f $(docker ps -qa)
-docker rmi -f $(docker images -q)
-docker volume rm $(docker volume ls -q)
-for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
+#!/bin/bash
+# Backup your data
+# Use at your own risk
+# Usage ./extended-cleanup-rancher2.sh
+# Include clearing all iptables: ./extended-cleanup-rancher2.sh flush
+containers=$(docker ps -qa)
+[[ ! -z "$containers" ]] && docker rm -f $containers
+images=$(docker images -q)
+[[ ! -z "$images" ]] && docker rmi -f $images
+volumes=$(docker volume ls -q)
+[[ ! -z "$volumes" ]] && docker volume rm $volumes
+for mount in $(mount | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
 cleanupdirs="/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico"
 for dir in $cleanupdirs; do
   echo "Removing $dir"
@@ -13,13 +20,16 @@ for interface in $cleanupinterfaces; do
   echo "Deleting $interface"
   ip link delete $interface
 done
-echo "flushing all iptables"
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -t nat -F
-iptables -t mangle -F
-iptables -F
-iptables -X
-echo "restarting docker"
-/etc/init.d/docker restart
+if [ "$1" = "flush" ]; then
+  echo "Parameter flush found, flushing all iptables"
+  iptables -F -t nat
+  iptables -X -t nat
+  iptables -F -t mangle
+  iptables -X -t mangle
+  iptables -F
+  iptables -X
+  service docker restart
+else
+  echo "Parameter flush not found, iptables not cleaned"
+fi
+
